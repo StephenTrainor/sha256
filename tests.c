515b5014f5f1efdb00ad011c99f464f9a9a7f163
StephenTrainor <stephontrainor@gmail.com>
2021-08-23 13:40:57 -0700

    Updated tests

--- tests.c
+++ tests.c
@@ -13,17 +13,9 @@ char *files[] = {
 };
 
 int main(void) {
-    uint32_t H[8];
-
-    for (unsigned int i = 0; i < sizeof(files) / sizeof(files[0]); i++) {
-        sha256(files[i], H);
-
-        printf("%s:\n", files[i]);
-        for (int j = 0; j < 8; j++) {
-            printf("%08x", H[j]);
-        }
-        printf("\n\n");
-
+    int i;
+    for (i = 0; i < 8; i++) {
+        sha256(files[i]);
     }
 
     return 0;
